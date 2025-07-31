eced9f824161df1de185ea4cd10fa9ebe65bde8c
Darren-Putra <darren.misyantono05@gmail.com>
2025-07-31 14:01:14 +0800

    buat seeder dan factory

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ['cat_name' => 'Makanan', 'description' => 'Kategori makanan'],
+            ['cat_name' => 'Minuman', 'description' => 'Kategori minuman'],
+            ['cat_name' => 'Snack', 'description' => 'Kategori snack'],
+        ];
+
+        // Insert the categories into the database
+        DB::table('categories')->insert($categories);
+    }
+}
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Roleseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Define the roles to be seeded
+        $roles = [
+            ['role_name' => 'Admin', 'description' => 'admin'],
+            ['role_name' => 'Casjier', 'description' => 'kasir'],
+            ['role_name' => 'Chef', 'description' => 'koki'],
+            ['role_name' => 'Customer', 'description' => 'pelanggan'],
+                
+        ];
+        DB::table('roles')->insert($roles);
+    }
+
+}
